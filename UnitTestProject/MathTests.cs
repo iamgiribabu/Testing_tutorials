ceec00c7790eb1c74c8e42eb185694a7c4f4a817
iamgiribabu <girianisetti06@gmail.com>
2022-10-27 22:29:41 +0530

    fundamental of NUnit testing

---
+++ UnitTestProject/MathTests.cs
@@ -0,0 +1,65 @@
+﻿
+using NUnit.Framework;
+using System;
+
+
+namespace UnitTestProject
+{
+    [TestFixture]
+    public class MathTests
+    {
+        private TestNinja.Fundamentals.Math _math;
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new TestNinja.Fundamentals.Math();
+        }
+
+        [Test]
+        
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            
+
+            var result = _math.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3)); 
+        }
+
+        [Test]
+        [TestCase(2, 1, 2)]
+        [TestCase(1, 2, 2)]
+        [TestCase(2, 2, 2)]
+        public void Max_WhenCalled_ReturnTheGreaterArgument(int a, int b, int exceptedResult)
+        {
+           
+
+            var result = _math.Max(a, b);
+
+            Assert.That(result, Is.EqualTo(exceptedResult));
+        }
+
+        [Test]
+        [Ignore("not in use")]
+        public void Max_SecondArgumentIsGreater_ReturnTheSecondtArgument()
+        {
+
+
+            var result = _math.Max(5, 10);
+
+            Assert.That(result, Is.EqualTo(10));
+        }
+        [Test]
+        [Ignore("not in use")]
+        public void Max_ArgumentsAreEqual_ReturnTheSameArgument()
+        {
+
+
+            var result = _math.Max(2, 2);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+
+    }
+}
--- UnitTestProject/RservationTests.cs
+++ UnitTestProject/RservationTests.cs
@@ -1,16 +1,62 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 using System;
 using TestNinja.Fundamentals;
 
 namespace UnitTestProject
 {
-    [TestClass]  //this attribute belongs to ms test framework
+    //[TestClass]  //this attribute belongs to ms test framework
+    //public class ReseervationTests
+    //{
+    //    //the first part specifies the name of the method on the test,
+    //    //the second part is the scenario of our testing, and 
+    //    //the third part is the expected behavior.
+    //    [TestMethod] //this attribute belongs to ms test framework
+    //    public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+    //    {
+    //        //Arrange :
+    //        //The Arrange part is where we initialize our objects.
+    //        var reseration = new Reservation();
+
+    //        //Act :
+    //        // the Act part is where we act on this object, and that basically means we're
+    //        //going to call a method, that's the method we're going to test. 
+
+    //        var result = reseration.CanBeCancelledBy(new User { IsAdmin = true });
+
+    //        //Assert
+
+    //        Assert.IsTrue(result);
+    //    }
+
+    //    [TestMethod]
+    //    public void CanBeCancelledBy_SameUserCancellingTheReseration_ReturnTrue()
+    //    {
+    //        var user = new User();
+    //        var reseration = new Reservation { MadeBy = user };
+    //        var result = reseration.CanBeCancelledBy(user);
+
+    //        Assert.IsTrue(result);
+    //    }
+
+    //    [TestMethod]
+    //    public void CanBeCancelledBy_AnotherUserCancellingTheReseration_ReturnFalse()
+    //    {
+
+    //        var reseration = new Reservation { MadeBy = new User() };
+    //        var result = reseration.CanBeCancelledBy(new User());
+
+    //        Assert.IsFalse(result);
+    //    }
+    //}
+
+    [TestFixture] 
     public class ReseervationTests
     {
         //the first part specifies the name of the method on the test,
         //the second part is the scenario of our testing, and 
         //the third part is the expected behavior.
-        [TestMethod] //this attribute belongs to ms test framework
+        [Test] 
         public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
         {
             //Arrange :
@@ -25,10 +71,14 @@ namespace UnitTestProject
 
             //Assert
 
+            // many ways to assert
+
             Assert.IsTrue(result);
+            //Assert.That(result , Is.True);  
+            //Assert.That(result == true);
         }
 
-        [TestMethod]
+        [Test]
         public void CanBeCancelledBy_SameUserCancellingTheReseration_ReturnTrue()
         {
             var user = new User();
@@ -38,10 +88,10 @@ namespace UnitTestProject
             Assert.IsTrue(result);
         }
 
-        [TestMethod]
+        [Test]
         public void CanBeCancelledBy_AnotherUserCancellingTheReseration_ReturnFalse()
         {
-           
+
             var reseration = new Reservation { MadeBy = new User() };
             var result = reseration.CanBeCancelledBy(new User());
 
