04b4872d87cba84958d5853e6dcbbf9eca958f70
iamgiribabu <girianisetti06@gmail.com>
2022-10-27 18:10:33 +0530

    Add project files.

---
+++ UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright © HP 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6525ee20-9416-4fb7-b413-196227186749")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TestNinja.Fundamentals;
+
+namespace UnitTestProject
+{
+    [TestClass]  //this attribute belongs to ms test framework
+    public class ReseervationTests
+    {
+        //the first part specifies the name of the method on the test,
+        //the second part is the scenario of our testing, and 
+        //the third part is the expected behavior.
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            //Arrange :
+            //The Arrange part is where we initialize our objects.
+            var reseration = new Reservation();
+
+            //Act :
+            // the Act part is where we act on this object, and that basically means we're
+            //going to call a method, that's the method we're going to test. 
+
+            var result = reseration.CanBeCancelledBy(new User { IsAdmin = true });
+
+            //Assert
+
+            Assert.IsTrue(result);
+        }
+    }
+}
