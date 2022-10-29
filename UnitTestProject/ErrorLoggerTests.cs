ceee84309513b0479499f1a11ad954a114b05521
iamgiribabu <girianisetti06@gmail.com>
2022-10-29 20:08:47 +0530

    dependency Injection via method parameter

--- UnitTestProject/ErrorLoggerTests.cs
+++ UnitTestProject/ErrorLoggerTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System;
 using TestNinja.Fundamentals;
 
 namespace UnitTestProject
@@ -25,5 +26,18 @@ namespace UnitTestProject
 
             Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
         }
+
+        [Test]
+        public void Log_ValidError_RaiseErrorLoggedEvent()
+        {
+            var logger = new ErrorLogger();
+            var id = Guid.Empty;
+            logger.ErrorLogged += (sender, args) => { id = args; };
+            logger.Log("a");
+
+            Assert.That(id, Is.Not.EqualTo(Guid.Empty));
+        }
+
+
     }
 }
---
+++ UnitTestProject/Mocking/VideoServiceTests.cs
@@ -0,0 +1,21 @@
+﻿
+using NUnit.Framework;
+using System;
+using TestNinja.Mocking;
+
+namespace UnitTestProject.Mocking
+{
+    [TestFixture]
+    public class VideoServiceTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            var service = new VideoService();
+
+            var result = service.ReadVideoTitle(new FakeFileReader());
+
+            Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+    }
+}
