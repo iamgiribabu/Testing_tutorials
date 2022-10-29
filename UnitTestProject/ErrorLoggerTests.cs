a802e66892d2a04b3c7c4e08858a066d1b0776d5
iamgiribabu <girianisetti06@gmail.com>
2022-10-29 16:51:18 +0530

    testing methods that throw exceptions

--- UnitTestProject/ErrorLoggerTests.cs
+++ UnitTestProject/ErrorLoggerTests.cs
@@ -15,5 +15,15 @@ namespace UnitTestProject
 
             Assert.That(logger.LastError, Is.EqualTo("a"));
         }
+        [Test]
+        [TestCase(null) ]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidError_ThrowArgumentNullException(string error)
+        {
+            var logger = new ErrorLogger();
+
+            Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
+        }
     }
 }
