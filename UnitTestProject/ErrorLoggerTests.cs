6a0eaf0d551c583976811a5b4f9d751f1af57776
iamgiribabu <girianisetti06@gmail.com>
2022-10-29 15:59:38 +0530

    testing void methods

---
+++ UnitTestProject/ErrorLoggerTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace UnitTestProject
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            var logger = new ErrorLogger();
+
+            logger.Log("a");
+
+            Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
+    }
+}
